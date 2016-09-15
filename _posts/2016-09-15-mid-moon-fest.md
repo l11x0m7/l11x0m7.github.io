1a20ff02c16af1d4c42b4e292b96856159123f48
l11x0m7 <l11x0m7@gmail.com>
2016-09-15 23:14:06 +0800

    refresh

---
+++ _posts/2016-09-15-mid-moon-fest.md
@@ -0,0 +1,9 @@
+--- 
+layout: post 
+title: 记中秋
+date: 2016-09-15 
+categories: blog 
+tags: [日记, 杂记] 
+description: 今年中秋做了这个个人博客，纪念一下。
+--- 
+
--- mkblog.sh
+++ mkblog.sh
@@ -4,4 +4,21 @@ read -p "input the article name with '-' as the interval: " artid
 
 today=`date "+%Y-%m-%d"`
 
-touch _posts/$today-$artid.md
+filename=$today-$artid.md
+pathname=_posts/$today-$artid.md
+if [[ ! -s $pathname ]];then
+    touch $pathname
+    echo "--- 
+layout: post 
+title: 
+date: $today 
+categories: blog 
+tags: [,] 
+description: 
+--- 
+" > $pathname
+else
+    echo -e "[Error] You've already created the file!"
+fi
+
+
