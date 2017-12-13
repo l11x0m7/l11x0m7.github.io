f010e149d4475ed8e78c2cc8d47ee98dfe98402f
lxm <l11x0m7@bupt.edu.cn>
2017-12-13 17:25:49 +0800

    refresh

--- mkblog.sh
+++ mkblog.sh
@@ -6,16 +6,16 @@ today=`date "+%Y-%m-%d"`
 
 filename=$today-$artid.md
 pathname=_posts/$today-$artid.md
-if [[ ! -s $pathname ]];then
+if [ ! -s $pathname ];then
     touch $pathname
-    echo "--- 
-layout: post 
-title: 
-date: $today 
-categories: blog 
-tags: [, ] 
-description: 
---- 
+    echo "---
+layout: post
+title:
+date: $today
+categories: blog
+tags: [, ]
+description:
+---
 " > $pathname
 else
     echo -e "[Error] You've already created the file!"
